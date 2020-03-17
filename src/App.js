2f243ec3e0c079c8a023649f5ac75134ead199cf
Potchara Puttawanchai <demon-angelo@hotmail.com>
2020-03-17 15:44:04 +0700

    Phase 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MapView from './Components/MapView';
+import 'leaflet/dist/leaflet.css';
+import './Css/App.scss';
+
+// API: https://coronavirus-tracker-api.herokuapp.com/all
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// States
+	const [latest, setLatest] = useState({
+		confirmed: 0,
+		recovered: 0,
+		deaths: 0,
+		updatedDate: null
+	});
+	const [locationArray, setLocationArray] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	// Functions
+	function getLatestCount(location, _locationArray) {
+		const foundLocation = _locationArray.find(_location => {
+			const matchCountry = _location.country === location.country;
+			const matchCountryCode = _location.country_code === location.countryCode;
+			const matchProvince = _location.province === location.province;
+			return matchCountry && matchCountryCode && matchProvince;
+		});
+		if (foundLocation === undefined) return 0;
+		return foundLocation.latest;
+	}
+
+	function combineLocationArray(confirmed, recovered, deaths) {
+		return confirmed.locations.map((location, index) => {
+			// Confirmed
+			const {
+				coordinates: { lat: _lat, long: _long },
+				country,
+				country_code: countryCode,
+				province,
+				latest: confirmedCount,
+				history
+			} = location;
+			const id = `${countryCode}-${index}`;
+			const coordinates = { lat: Number(_lat), long: Number(_long) }
+			const newLocation = { id, coordinates, country, countryCode, province, confirmedCount, history };
+
+			// Recovered, Deaths
+			newLocation.recoveredCount = getLatestCount(newLocation, recovered.locations);
+			newLocation.deathsCount = getLatestCount(newLocation, deaths.locations);
+
+			return newLocation;
+		})
+	}
+
+	// Effects
+	useEffect(() => {
+		axios.get('https://coronavirus-tracker-api.herokuapp.com/all').then(response => {
+			console.log(response.data);
+			const { confirmed, recovered, deaths, latest } = response.data;
+			setLocationArray(combineLocationArray(confirmed, recovered, deaths));
+		}).catch(error => {
+			console.error(error);
+		})
+	}, []);
+
+	return (
+		<div className="App">
+			<MapView center={[13, 100]} zoom={5} locationArray={locationArray} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/MapView.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { divIcon } from 'leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+/*
+location confirmed count => icon style
+1-100 => pink, super very small
+100-500 => pink, very small
+501-1000 => pink, small
+1001-5000 => purple, normal
+5001-10000 => purple, big
+10001-50000 => red, very big
+50000 up => red, super very big
+*/
+const markerIcons = {
+	xxSmall: divIcon({
+		className: 'case__marker pink', iconSize: [10, 10],
+	}),
+	xSmall: divIcon({
+		className: 'case__marker pink', iconSize: [16, 16]
+	}),
+	small: divIcon({
+		className: 'case__marker pink', iconSize: [24, 24]
+	}),
+	normal: divIcon({
+		className: 'case__marker purple', iconSize: [32, 32]
+	}),
+	large: divIcon({
+		className: 'case__marker purple', iconSize: [48, 48]
+	}),
+	xLarge: divIcon({
+		className: 'case__marker red', iconSize: [72, 72]
+	}),
+	xxLarge: divIcon({
+		className: 'case__marker red', iconSize: [96, 96]
+	})
+};
+
+function MapView(props) {
+    const { locationArray, center, zoom } = props;
+    // Elements
+    const markerElements = locationArray.map(location => {
+        const {
+            id, coordinates: { lat, long },
+            country, province, confirmedCount
+        } = location;
+
+        let markerIcon = markerIcons.xxSmall
+        if (confirmedCount >= 101 && confirmedCount <= 500) {
+            markerIcon = markerIcons.xSmall;
+        }
+        else if (confirmedCount >= 501 && confirmedCount <= 1000) {
+            markerIcon = markerIcons.small;
+        }
+        else if (confirmedCount >= 1001 && confirmedCount <= 5000) {
+            markerIcon = markerIcons.normal;
+        }
+        else if (confirmedCount >= 5001 && confirmedCount <= 10000) {
+            markerIcon = markerIcons.large;
+        }
+        else if (confirmedCount >= 10001 && confirmedCount <= 50000) {
+            markerIcon = markerIcons.xLarge;
+        }
+        else if (confirmedCount >= 50001) {
+            markerIcon = markerIcons.xxLarge;
+        }
+
+        const label = (province !== '') ? `${province}, ${country}` : country;
+
+        return (
+            <Marker
+                key={id}
+                position={[lat, long]}
+                icon={markerIcon}
+                onmouseover={e => e.target.openPopup()}
+                onmouseout={e => e.target.closePopup()} >
+                <Popup>
+                    <b>{label}</b>
+                </Popup>
+            </Marker>
+        );
+    });
+
+    return (
+        <Map className="mapview" center={center} zoom={zoom}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            {markerElements}
+        </Map>
+    )
+}
+
+export default MapView;
