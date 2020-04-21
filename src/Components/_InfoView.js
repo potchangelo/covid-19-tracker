d69cc286f9a291e974d047afc8c81963dc40316e
Potchara Puttawanchai <demon-angelo@hotmail.com>
2020-04-21 17:36:19 +0700

    Extract modal to be layout

--- src/Components/_InfoView.js
+++ src/Components/_InfoView.js
@@ -1,27 +1,26 @@
 import './Css/InfoView.scss';
 import React from 'react';
+import { Modal } from '../Layouts';
 
 function InfoView(props) {
     const { isShow, onClickClose } = props;
-
-    if (!isShow) return null;
-
     return (
-        <div className="info-view modal is-active">
-            <div className="modal-background" onClick={onClickClose}></div>
-            <div className="info-view__content modal-content has-text-centered">
-                <div className="columns is-mobile">
-                    <div className="column">
-                        <a href="https://github.com/potchangelo/covid-19-tracker" target="_blank" rel="noopener noreferrer">Project Github</a>
-                    </div>
-                    <div className="column">
-                        <a href="https://github.com/ExpDev07/coronavirus-tracker-api" target="_blank" rel="noopener noreferrer">Data API Github</a>
-                    </div>
+        <Modal 
+            extraClass="info-view" 
+            extraContentClass="info-view__content has-text-centered"
+            isShow={isShow} 
+            onClickClose={onClickClose}
+            >
+            <div className="columns is-mobile">
+                <div className="column">
+                    <a href="https://github.com/potchangelo/covid-19-tracker" target="_blank" rel="noopener noreferrer">Project Github</a>
                 </div>
-                <p className="is-size-7">&copy; 2020 Zinglecode</p>
+                <div className="column">
+                    <a href="https://github.com/ExpDev07/coronavirus-tracker-api" target="_blank" rel="noopener noreferrer">Data API Github</a>
+                </div>
             </div>
-            <button className="modal-close is-large" onClick={onClickClose}></button>
-        </div>
+            <p className="is-size-7">&copy; 2020 Zinglecode</p>
+        </Modal>
     );
 }
 
