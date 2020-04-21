475706a0e4eb3b46011380053cad0d6e5c9dcc59
Potchara Puttawanchai <demon-angelo@hotmail.com>
2020-04-21 17:45:19 +0700

    Add error message view

---
+++ src/Components/_ErrorView.js
@@ -0,0 +1,23 @@
+import './Css/ErrorView.scss';
+import React from 'react';
+
+function ErrorView(props) {
+    const { error, onClickClose } = props;
+
+    let label = 'Something went wrong, please wait and try again.'
+    if (error === null) return null;
+    else if (error.message === 'Network Error') {
+        label = 'API network error, please wait and try again.'
+    }
+
+    return (
+        <div className="error-view">
+            <div className="notification is-danger is-light">
+                <button className="delete" onClick={onClickClose}></button>
+                <p><b>Error : </b>{label}</p>
+            </div>
+        </div>
+    );
+}
+
+export default ErrorView;
