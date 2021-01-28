16f296dfb62f74f0165e83afe5fe5c228adc69bb
Potchara Puttawanchai <potchangelo@gmail.com>
2021-01-28 16:25:43 +0700

    Update info view content

--- src/Components/_InfoView.js
+++ src/Components/_InfoView.js
@@ -13,19 +13,35 @@ function InfoView(props) {
     return (
         <Modal 
             extraClass="info-view" 
-            extraContentClass="info-view__content has-text-centered"
+            extraContentClass="info-view__content"
             isShow={isShow} 
             onClickClose={unsetModal}
-            >
-            <div className="columns is-mobile">
-                <div className="column">
-                    <a href="https://github.com/potchangelo/covid-19-tracker" target="_blank" rel="noopener noreferrer">Project Github</a>
-                </div>
-                <div className="column">
-                    <a href="https://github.com/ExpDev07/coronavirus-tracker-api" target="_blank" rel="noopener noreferrer">Data API Github</a>
-                </div>
+        >
+            <div className="content">
+                <h6 className="title is-6">Marker colors</h6>
+                <p>
+                    <span className="info-view__color-marker pink"></span>
+                    <span className="info-view__color-text">0 - 50,000 confirmed cases.</span>
+                </p>
+                <p>
+                    <span className="info-view__color-marker purple"></span>
+                    <span className="info-view__color-text">50,001 - 500,000 confirmed cases.</span>
+                </p>
+                <p>
+                    <span className="info-view__color-marker red"></span>
+                    <span className="info-view__color-text">500,001+ confirmed cases.</span>
+                </p>
             </div>
-            <p className="is-size-7">&copy; 2021 Zinglecode</p>
+            <div className="content">
+                <h6 className="title is-6">Links</h6>
+                <p>
+                    <a href="https://github.com/potchangelo/covid-19-tracker" target="_blank" rel="noopener noreferrer">Project Github</a>
+                </p>
+                <p>
+                    <a href="https://github.com/ExpDev07/coronavirus-tracker-api" target="_blank" rel="noopener noreferrer">Data API Github</a>
+                </p>
+            </div>
+            <p className="is-size-7">&copy; Copyright 2021 Zinglecode.</p>
         </Modal>
     );
 }
