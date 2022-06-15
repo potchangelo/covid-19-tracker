923f961b146b9f8e4937a59c87450c5225ec9877
Potchara Puttawanchai <potchangelo@gmail.com>
2022-06-15 12:37:22 +0700

    Update API base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import Axios from 'axios';
 
 // Old API: https://coronavirus-tracker-api.herokuapp.com/all
-const baseUrl = 'https://coronavirus-tracker-api.herokuapp.com/v2';
+// Out of reach API: https://coronavirus-tracker-api.herokuapp.com/v2
+// New baseUrl from https://github.com/Kilo59/coronavirus-tracker-api
+const baseUrl = 'https://covid-tracker-us.herokuapp.com/v2';
 
 const api = {
   getAllLocation: () => Axios.get(`${baseUrl}/locations`),
