a0eae9dfbd78fb26331c10dd4b2db4d2e2462387
Potchara Puttawanchai <potchangelo@gmail.com>
2022-09-13 15:27:19 +0700

    Remove unused Redux modal parts

--- src/redux/filters/actionThunk.js
+++ src/redux/filters/actionThunk.js
@@ -1,24 +1,24 @@
-import { setFilter, cancelFilter, resetFilter } from './action';
-import { unsetSelectedLocation } from '../locations/action';
-import { unsetModal } from '../modal/action';
-import { unsetError } from '../error/action';
+// import { setFilter, cancelFilter, resetFilter } from './action';
+// import { unsetSelectedLocation } from '../locations/action';
+// import { unsetModal } from '../modal/action';
+// import { unsetError } from '../error/action';
 
-const applySetFilter = () => dispatch => {
-  dispatch(setFilter());
-  dispatch(unsetSelectedLocation());
-  dispatch(unsetModal());
-  dispatch(unsetError());
-};
+// const applySetFilter = () => dispatch => {
+//   dispatch(setFilter());
+//   dispatch(unsetSelectedLocation());
+//   dispatch(unsetModal());
+//   dispatch(unsetError());
+// };
 
-const applyCancelFilter = () => dispatch => {
-  dispatch(cancelFilter());
-  dispatch(unsetModal());
-};
+// const applyCancelFilter = () => dispatch => {
+//   dispatch(cancelFilter());
+//   dispatch(unsetModal());
+// };
 
-const applyResetFilter = () => dispatch => {
-  dispatch(resetFilter());
-  dispatch(unsetSelectedLocation());
-  dispatch(unsetError());
-};
+// const applyResetFilter = () => dispatch => {
+//   dispatch(resetFilter());
+//   dispatch(unsetSelectedLocation());
+//   dispatch(unsetError());
+// };
 
-export { applySetFilter, applyCancelFilter, applyResetFilter };
+// export { applySetFilter, applyCancelFilter, applyResetFilter };
